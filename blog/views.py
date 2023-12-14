33b813ab48d5dceab80a5b6e0038e7573ae63c76
ParsaMohajeri <parsamohajeri1188@gmail.com>
2023-12-14 19:35:42 +0330

    fixed problem of view

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def blog_view(request):
     return render(request,'blog/blog-home.html')
 def blog_single(request):
-    return render(request,'blog/blog-single')
+    return render(request,'blog/blog-single.html')
