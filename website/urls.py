d1f81c68888203ea2ec81528e93f3f8dcbe7d11a
ParsaMohajeri <parsamohajeri1188@gmail.com>
2023-12-14 18:51:42 +0330

    fixed problems
    fixed oserror and other

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from website.views import *
 
+app_name = 'website'
+
 urlpatterns = [
-    path('',index_view),
-    path('about',about_view),
-    path('contact',contact_view)
 
 ]
