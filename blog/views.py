cc061d720e80a8964457268212e655be4bffe25f
ParsaMohajeri <parsamohajeri1188@gmail.com>
2023-12-27 18:30:03 +0330

    you can see posts in website
    now you can see the real posts in website and I created the published_date to show posts until the time come

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post
+import datetime
+from django.utils import timezone
 
 # Create your views here.
 def blog_view(request):
-    return render(request,'blog/blog-home.html')
+    posts=Post.objects.filter(status=1)
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    # posts=Post.objects.filter(status=0)
+    # posts=Post.objects.filter(published_date=datetime.date(2023,12,24))
+    # start_date = datetime.date(2005, 1, 1)
+    # end_date = datetime.date(2022, 12, 27)
+    # pub_d=Post.objects.filter(published_date=(start_date,end_date))
+    context={'posts':posts}
+    return render(request,'blog/blog-home.html',context)
 def blog_single(request):
     return render(request,'blog/blog-single.html')
+def test(request,pid):
+    # post=Post.objects.get(id=pid)
+    # posts=Post.objects.all()
+    # posts=Post.objects.filter(status=0)
+    post = get_object_or_404(Post,pk=pid)
+    context={'post':post}
+    return render(request,'test.html',context)
