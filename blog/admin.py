093a14e3b96f427d0f267b125d0f20a59adc81f4
ParsaMohajeri <parsamohajeri1188@gmail.com>
2023-12-28 14:45:07 +0330

    added category and fixed requirements

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post,Category
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
@@ -11,4 +11,6 @@ class PostAdmin(admin.ModelAdmin):
     search_fields=['title','content']
     # fields=('title',) 
     # exclude=('content',)
+
+admin.site.register(Category)
 admin.site.register(Post,PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
-from django.db.models import F
-
 from django.utils import timezone
 
 # Create your views here.
@@ -24,6 +22,7 @@ def blog_single(request,pid):
     post = get_object_or_404(posts,pk=pid)
     post.counted_views += 1
     post.save()
+    
     context={'post':post}
     return render(request,'blog/blog-single.html',context)
 def test(request,pid):
