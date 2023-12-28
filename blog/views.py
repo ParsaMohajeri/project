5fd1ee3f1c7ec07a5acf6b6957dfb4f67cc9bf45
ParsaMohajeri <parsamohajeri1188@gmail.com>
2023-12-28 12:52:18 +0330

    added real counted_view

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,8 @@ from django.utils import timezone
 
 # Create your views here.
 def blog_view(request):
-    posts=Post.objects.filter(status=1)
-    posts = Post.objects.filter(published_date__lte=timezone.now())
+    posts=Post.objects.filter(status=1,published_date__lte=timezone.now())
+
 
     # for posts in Post.objects.all():
     #     Post.counted_views  =+1
@@ -20,10 +20,10 @@ def blog_view(request):
     context={'posts':posts}
     return render(request,'blog/blog-home.html',context)
 def blog_single(request,pid):
-    posts=Post.objects.filter(status=1)
-    posts=Post.objects.filter(status=1)
-    posts = Post.objects.filter(published_date__lte=timezone.now())
+    posts=Post.objects.filter(status=1,published_date__lte=timezone.now())
     post = get_object_or_404(posts,pk=pid)
+    post.counted_views += 1
+    post.save()
     context={'post':post}
     return render(request,'blog/blog-single.html',context)
 def test(request,pid):
