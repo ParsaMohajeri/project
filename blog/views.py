dfee47ee6b66323e78369b2286086c165a5097e0
ParsaMohajeri <parsamohajeri1188@gmail.com>
2023-12-28 12:04:22 +0330

    added a lot of things
    I added image and author changes and other migrations and I hope I added dbsqlite to

--- blog/views.py
+++ blog/views.py
@@ -9,8 +9,8 @@ def blog_view(request):
     posts=Post.objects.filter(status=1)
     posts = Post.objects.filter(published_date__lte=timezone.now())
 
-    for posts in Post.objects.all():
-        Post.counted_views  =+1
+    # for posts in Post.objects.all():
+    #     Post.counted_views  =+1
     # return render(request, 'blog/post_detail.html', {'post': post})
     # posts=Post.objects.filter(status=0)
     # posts=Post.objects.filter(published_date=datetime.date(2023,12,24))
@@ -19,8 +19,13 @@ def blog_view(request):
     # pub_d=Post.objects.filter(published_date=(start_date,end_date))
     context={'posts':posts}
     return render(request,'blog/blog-home.html',context)
-def blog_single(request):
-    return render(request,'blog/blog-single.html')
+def blog_single(request,pid):
+    posts=Post.objects.filter(status=1)
+    posts=Post.objects.filter(status=1)
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    post = get_object_or_404(posts,pk=pid)
+    context={'post':post}
+    return render(request,'blog/blog-single.html',context)
 def test(request,pid):
     # post=Post.objects.get(id=pid)
     # posts=Post.objects.all()
