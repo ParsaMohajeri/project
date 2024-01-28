b9730b608e50dd974035f4e741cd11a38400e4e6
ParsaMohajeri <parsamohajeri1188@gmail.com>
2024-01-28 21:43:36 +0330

    added some codes to template tag

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 from django.utils import timezone
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 
 # Create your views here.
-def blog_view(request):
+def blog_view(request,**kwargs):
     posts=Post.objects.filter(status=1,published_date__lte=timezone.now())
+    if kwargs.get('cat_name')!=None:
+        posts=posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username')!=None:
+        posts=posts.filter(author__username=kwargs['author_username'])
+    
+
+    posts=Paginator(posts,2)
+    try:
+        page_number=request.GET.get('page')
+        posts=posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts=posts.get_page(1)
+    except EmptyPage :
+        posts=posts.get_page(1)
+
+    
     # return render(request, 'blog/post_detail.html', {'post': post})
     # posts=Post.objects.filter(status=0)
     context={'posts':posts}
@@ -31,3 +48,22 @@ def test(request):
     # post = get_object_or_404(Post,pk=pid)
     # context={'post':post}
     return render(request,'test.html')
+
+
+def blog_category(request,cat_name):
+    posts=Post.objects.filter(status=1)
+    posts=posts.filter(category__name=cat_name)
+    context={'posts':posts}
+    return render(request,'blog/blog-home.html',context)
+
+def blog_search(request):
+    posts=Post.objects.filter(status=1,published_date__lte=timezone.now())
+
+    # print(request.__dict__)
+    if request.method=='GET':
+        # print(request.GET.get('s'))
+        if s:=request.GET.get('s'):
+            posts=posts.filter(content__contains=s)
+    #     print('get request')
+    context={'posts':posts}
+    return render(request,'blog/blog-home.html',context)
